0cdfc4152321623833d1c3b4e7f6bf1a7fa6560a
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-28 21:09:13 +0200

    Add posts CRUD Add create post and list post pages

--- src/Controllers/PostsController.php
+++ src/Controllers/PostsController.php
@@ -3,15 +3,50 @@
 namespace App\Controllers;
 
 use App\DB;
+use App\Models\Post;
+use App\Models\User;
 
 class PostsController
 {
     public function index()
     {
-       $db = new DB();
-       $posts = $db->all('posts');
-       dump($posts);
-       $users = $db->all('users');
-       dump($users);
+       $posts = Post::all();
+       view('posts/index', compact('posts'));
+    }
+
+    public function create() {
+        view('posts/create');
+    }
+
+    public function store() {
+        $post = new Post();
+        $post->title = $_POST['title'];
+        $post->body = $_POST['body'];
+        $post->save();
+        redirect('/posts');
+    }
+
+    public function view() {
+        $post = Post::find($_GET['id']);
+        view('posts/view', compact('post'));
+    }
+
+    public function edit() {
+        $post = Post::find($_GET['id']);
+        view('posts/edit', compact('post'));
+    }
+
+    public function update() {
+        $post = Post::find($_GET['id']);
+        $post->title = $_POST['title'];
+        $post->body = $_POST['body'];
+        $post->save();
+        redirect('/posts');
+    }
+
+    public function destroy() {
+        $post = Post::find($_GET['id']);
+        $post->delete();
+        redirect('/posts');
     }
 }
---
+++ views/posts/index.php
@@ -0,0 +1,27 @@
+<?php include __DIR__ . '/../partials/header.php'; ?>
+<main class="container">
+    <a class="btn btn-primary" href="/posts/create">New Post</a>
+    <table class="table table-striped table-hover">
+        <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Actions</th>
+        </thead>
+        <tbody>
+            <?php foreach ($posts as $post): ?>
+                <tr>
+                    <td><?= $post->id ?></td>
+                    <td><?= $post->title ?></td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <a class="btn btn-info" href="/posts/view?id=<?=$post->id?>">View</a>
+                            <a class="btn btn-warning" href="/posts/edit?id=<?=$post->id?>">Edit</a>
+                            <a class="btn btn-danger" href="/posts/delete?id=<?=$post->id?>">Delete</a>
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
+<?php include __DIR__ . '/../partials/footer.php'; ?>
