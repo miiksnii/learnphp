1c8bb43139924f1667c8819d7ff5c4e389fd69da
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-28 20:58:23 +0200

    added us

--- us.php
+++ us.php
@@ -1,38 +1,33 @@
 <?php
 $posts = [
   [
-    'title' => 'World news 1',
+    'title' => 'U.S news 1',
     'published' => '16.09.2025',
     'author' => 'Kaspar',
-    'body' => 'Some world news body 1'
+    'body' => 'Some U.S news body 1'
   ],
   [
-    'title' => 'World news 2',
+    'title' => 'U.S news 2',
     'published' => '15.09.2025',
     'author' => 'Martin',
-    'body' => 'Some world news body 2'
+    'body' => 'Some U.S news body 2'
   ],
   [
-    'title' => 'World news 3',
+    'title' => 'U.S news 3',
     'published' => '14.09.2025',
     'author' => 'Pets',
-    'body' => 'Some world news body 3'
+    'body' => 'Some U.S news body 3'
   ],
   [
-    'title' => 'World news 4',
+    'title' => 'U.S news 4',
     'published' => '13.09.2025',
     'author' => 'Kelly',
-    'body' => 'Some world news body 4'
+    'body' => 'Some U.S news body 4'
   ],
 ];
 ?>
-
-
 <?php include __DIR__ . '/partials/header.php'; ?>
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
-
       <div class="row g-5">
         <div class="col-md-8">
             <?php include __DIR__ . '/partials/posts.php'; ?>
